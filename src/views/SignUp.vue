8a6325f38f98be2f4d6d818513ccd8f75d50a5ed
akshata3113 <akshatashejwal2004@gmail.com>
2024-12-28 17:36:07 +0530

    vue js project setup

---
+++ src/views/SignUp.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+    <div class="about">
+      <h1>Sign Up</h1>
+    </div>
+    <div>
+      <Button>Sign Up</Button>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
